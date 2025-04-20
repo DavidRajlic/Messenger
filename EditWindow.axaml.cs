51d230faf1f81d115ba0dfd24a982c679be25ce9
DavidRajlic <rajlic.david@gmail.com>
2025-04-20 23:22:40 +0200

    image pointer added

--- EditWindow.axaml.cs
+++ EditWindow.axaml.cs
@@ -20,6 +20,30 @@ namespace Messenger
         {
             this.Close(); 
         }
+
+        private async void Image_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                AllowMultiple = false,
+                Filters = new List<FileDialogFilter>
+                {
+                    new FileDialogFilter { Name = "Slike", Extensions = { "jpg", "jpeg", "png" } }
+                }
+            };
+
+            var result = await dialog.ShowAsync(this);
+
+            if (result != null && result.Length > 0)
+            {
+                string selectedImagePath = result[0];
+
+                if (DataContext is MainViewModel vm && vm.SelectedUser != null)
+                {
+                    vm.SelectedUser.ProfilePicturePath = selectedImagePath;
+                }
+            }
+        }
         
     }
 }
